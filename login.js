92be3cf1fcf67981ab23686cb38dffed284d66a5
moto114 <42561840+moto114@users.noreply.github.com>
2020-10-27 00:40:13 +0900

    Update login.js

--- login.js
+++ login.js
@@ -2,16 +2,5 @@ document.getElementById("login_form").onsubmit = function()
 {
     tmp();
     console.log("onsubmit!!?????");
-    chrome.storage.sync.set(
-        {
-            username : document.getElementById("username").value
-        }
-    )
-
     return false;
 }
-
-chrome.storage.sync.get(["username"], function (items) {
-    document.getElementById("username").value = items.username; // -> "string1"
-
-});
